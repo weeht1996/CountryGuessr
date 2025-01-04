5b58974b497c3da40bfc172bec200491e0e5d305
weeht1996 <72547656+weeht1996@users.noreply.github.com>
2025-01-04 20:11:51 +0800

    Updated attributions and Header

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,17 @@
 const Footer = () => {
     return (
-        <div className="flex w-full items-center justify-center text-slate-400 text-clamp">
-            <div className="Thanks w-1/5 flex flex-col">
-                <b className="text-clamp-l">Special Thanks</b>
-                <span>If it ain't obvious enough, idea, layout & designs from: <link rel="stylesheet" href="https://boxofficega.me/"/></span>
-            </div>
-            <div className="attributions">
-                <b>Attributions: </b> <br />
-                <span>https://freevectormaps.com/world-maps/WRLD-EPS-02-0007?ref=atr</span>
+        <div className="footer-container w-full flex justify-center">
+            <div className="footer flex w-11/12 items-center text-slate-400 text-clamp">
+                <div className="thanks w-1/2 flex flex-col items-center">
+                    <b className="text-clamp-l mx-2">Special Thanks</b>
+                    <span>Idea, layout & designs from: <a href="https://boxofficega.me/">Box Office Game</a></span>
+                </div>
+                <div className="attributions w-1/2 flex flex-col items-center">
+                    <b className="text-clamp-l mx-2">Attributions:</b>
+                    <span>Map by <a href="https://react-leaflet.js.org/">React Leaflet</a></span>
+                    <span>Country Information by <a href="https://restcountries.com/">Rest API</a></span>
+                    <span>GDP information by <a href="https://documents.worldbank.org/en/publication/documents-reports/api/">The World Bank Group</a></span>
+                </div>
             </div>
         </div>
 
