d9702b7e0ed7fa353d1bc48f0cabc1906674a50f
weeht1996 <72547656+weeht1996@users.noreply.github.com>
2025-01-05 15:14:56 +0800

    basename update router

--- src/index.tsx
+++ src/index.tsx
@@ -19,13 +19,10 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/CountryGuessr'>
       <App />
     </BrowserRouter>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
